5891c84175a987c2ccaeb51a7cbcc4558b2322cc
Jonathan <jonathancozzo@gmail.com>
2012-10-26 14:14:39 +0200

    Création classe Coup

---
+++ TP Cpp 2012/Coup.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Coup.cpp
+ * Author: jonathancozzo
+ * 
+ * Created on 26 octobre 2012, 14:13
+ */
+
+#include "Coup.h"
+
+Coup::Coup() {
+}
+
+Coup::Coup(const Coup& orig) {
+}
+
+Coup::~Coup() {
+}
+
---
+++ TP Cpp 2012/Coup.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Coup.h
+ * Author: jonathancozzo
+ *
+ * Created on 26 octobre 2012, 14:13
+ */
+
+#ifndef COUP_H
+#define	COUP_H
+
+class Coup {
+public:
+    Coup();
+    Coup(const Coup& orig);
+    virtual ~Coup();
+private:
+
+};
+
+#endif	/* COUP_H */
+
