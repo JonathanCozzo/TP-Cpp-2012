e2de4b05aa569a921cb12f3938155d85bbc8de4c
Jonathan <jonathancozzo@gmail.com>
2012-11-05 14:25:28 +0100

    Création classe Joueur

---
+++ TP Cpp 2012/Joueur.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Joueur.cpp
+ * Author: jonathancozzo
+ * 
+ * Created on 5 novembre 2012, 14:17
+ */
+
+#include "Joueur.h"
+
+Joueur::Joueur() {
+}
+
+Joueur::Joueur(const Joueur& orig) {
+}
+
+Joueur::~Joueur() {
+}
+
---
+++ TP Cpp 2012/Joueur.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Joueur.h
+ * Author: jonathancozzo
+ *
+ * Created on 5 novembre 2012, 14:17
+ */
+
+#ifndef JOUEUR_H
+#define	JOUEUR_H
+
+class Joueur {
+public:
+    Joueur();
+    Joueur(const Joueur& orig);
+    virtual ~Joueur();
+private:
+
+};
+
+#endif	/* JOUEUR_H */
+
